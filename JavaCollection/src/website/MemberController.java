d24ec99c336b709ea0950ac53060462a27c518be
jinyongshin <jyshin93@Hotmail.com>
2015-07-30 15:21:10 +0900

    2015.07.30

--- JavaCollection/src/website/MemberController.java
+++ JavaCollection/src/website/MemberController.java
@@ -15,19 +15,8 @@ public class MemberController {
 				break;
 			} else {
 				switch (input) {
-				case "join": //회원가입
-					System.out.println("userid를 입력하세요");
-					String id = scanner.next();
-					System.out.println("비밀번호:");
-					String pass = scanner.next();
-					System.out.println("이름:");
-					String name = scanner.next();
-					System.out.println("나이:");
-					int age = scanner.nextInt();
-					System.out.println("주소입력:");
-					String address = scanner.next();
-					service.join(id, pass, name, age, address);
-					System.out.println("회원가입 성공");					
+				case "join": // 메소드 리팩토링 단축키 ALT SHIFT M
+					join(service, scanner);					
 					break;
 				case "login": // 로그인
 					System.out.println("아이디:");
@@ -40,11 +29,13 @@ public class MemberController {
 						System.out.println(result);
 						break;
 					}else if (flag.equals("비번이 다")) {
-						
+						System.out.println(result);
+						break;
 					}else {
-						
+						System.out.println(result);
+						break;
 					}
-					break;
+				
 
 				default:
 					System.out.println("잘못 입력하였습니다.");
@@ -52,5 +43,20 @@ public class MemberController {
 				}
 			}
 		}
+	}
+
+	private static void join(MemberService service, Scanner scanner) {
+		System.out.println("userid를 입력하세요");
+		String id = scanner.next();
+		System.out.println("비밀번호:");
+		String pass = scanner.next();
+		System.out.println("이름:");
+		String name = scanner.next();
+		System.out.println("나이:");
+		int age = scanner.nextInt();
+		System.out.println("주소입력:");
+		String address = scanner.next();
+		service.join(id, pass, name, age, address);
+		System.out.println("회원가입 성공");
 	}
 }
